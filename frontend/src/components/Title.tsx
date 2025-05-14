d3308ae6d8244e0067daf7ebb7dae7e20028d9d0
Zach Coursey <zcoursey22@gmail.com>
2025-05-13 23:00:45 -0400

    Tools setup work

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,13 +1,13 @@
 import { Flex, Group, Heading } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
-import viteLogo from "../assets/vite.svg";
+import logoSvg from "../assets/images/logo.svg";
 
 export const Title = () => {
   return (
     <Flex asChild align={"center"} justify={"space-between"}>
       <NavLink to="/">
         <Group>
-          <img src={viteLogo} alt="Etudio logo" />
+          <img src={logoSvg} alt="Etudio logo" />
           <Heading
             size="3xl"
             fontFamily={"monospace"}
--- frontend/src/components/tools/Tools.tsx
+++ frontend/src/components/tools/Tools.tsx
@@ -1,22 +1,56 @@
-import { Accordion, Heading, Stack } from "@chakra-ui/react";
+import { Accordion, Group, Heading, Icon, Stack, Text } from "@chakra-ui/react";
 import { LuGauge, LuHourglass } from "react-icons/lu";
 import { TbMetronome } from "react-icons/tb";
-import { ToolItem, ToolItemProps } from "./ToolItem";
+import useLocalStorage from "use-local-storage";
+import { ReactNode } from "react";
 
-const tools: ToolItemProps[] = [
-  { value: "metronome", label: "Metronome", icon: TbMetronome },
-  { value: "pomodoro", label: "Pomodoro", icon: LuHourglass },
-  { value: "tuner", label: "Tuner", icon: LuGauge },
+export interface ToolItemConfig {
+  value: string;
+  label: string;
+  element?: ReactNode;
+  icon?: ReactNode;
+}
+
+const tools: ToolItemConfig[] = [
+  { value: "metronome", label: "Metronome", icon: <TbMetronome /> },
+  { value: "pomodoro", label: "Pomodoro", icon: <LuHourglass /> },
+  { value: "tuner", label: "Tuner", icon: <LuGauge /> },
 ];
 
+const KEY = "etudio__open_tools";
+
 export const Tools = () => {
+  const [openTools, setOpenTools] = useLocalStorage<string[]>(KEY, []);
+
   return (
     <Stack gap={"0"}>
       <Heading>Tools</Heading>
-      <Accordion.Root multiple>
-        {tools.map((tool) => (
-          <ToolItem key={tool.value} {...tool} />
-        ))}
+      <Accordion.Root
+        multiple
+        value={openTools}
+        onValueChange={({ value }) => setOpenTools(value)}
+      >
+        {tools.map(({ value, label, icon, element }) => {
+          const isOpen = openTools.includes(value);
+          return (
+            <Accordion.Item key={value} value={value}>
+              <Accordion.ItemTrigger cursor={"pointer"}>
+                <Group flex={"1"}>
+                  {icon && (
+                    <Icon color={isOpen ? "fg.success" : "fg.subtle"}>
+                      {icon}
+                    </Icon>
+                  )}
+                  <Text>{label}</Text>
+                </Group>
+                <Accordion.ItemIndicator />
+              </Accordion.ItemTrigger>
+              <Accordion.ItemContent>
+                <Accordion.ItemBody>{element}</Accordion.ItemBody>
+              </Accordion.ItemContent>
+            </Accordion.Item>
+          );
+        })}
       </Accordion.Root>
     </Stack>
   );
