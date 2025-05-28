d924e37746e30523147d928951542c1489d064f3
Zach Coursey <zcoursey22@gmail.com>
2025-05-28 14:19:16 -0400

    Redirect from delete on goal detail page

--- frontend/src/components/resources/goals/goalActions.tsx
+++ frontend/src/components/resources/goals/goalActions.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useDeleteGoal } from "../../../hooks";
 import { Goal } from "../../../models";
 import {
@@ -6,14 +7,22 @@ import {
   editActionConfigMap,
   resolveActions,
 } from "../shared";
+import { getGoalListPath } from "../../../routes";
 
 export const useGoalActions = (overrides?: ActionOverrides<Goal>) => {
   const { deleteResource } = useDeleteGoal();
+  const navigate = useNavigate();
+  const listPath = getGoalListPath();
 
   return resolveActions<Goal>(
     {
       ...editActionConfigMap(({ name }) => console.log(`Edit ${name}`)),
-      ...deleteActionConfigMap(({ id }) => deleteResource(id)),
+      ...deleteActionConfigMap(({ id }) => {
+        deleteResource(id);
+        if (location.pathname.startsWith(`${listPath}/`)) {
+          navigate(listPath, { replace: true });
+        }
+      }),
     },
     overrides
   );
